6a3ac6b68f88ad56c42dfd77cf0ce07e4182ba30
pancudaniel7 <pancudaniel7@gmail.com>
2022-10-30 22:09:02 +0200

    Changed truffle config

--- truffle.js
+++ truffle.js
@@ -1,8 +1,11 @@
 module.exports = {
     networks: {
         production: {
-            host: "mainnet.infura.io/v3/16d9f54291304226ba7789dc15e98c6e",
-            network_id: 1
+            host:"mainnet.infura.io",
+            network_id: 1,
+	    port: 80,
+	    from: "/v3/16d9f54291304226ba7789dc15e98c6e",
+	    gas: 300000,
         }
     }
 }
